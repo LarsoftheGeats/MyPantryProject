463e1e0d7c99f5cf9a7af936ca7302a462391f5d
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-11-09 10:37:18 -0700

    running cors

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const path = require('path')
 require('dotenv').config()
 const port = process.env.PORT || 5050
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 const {landingPage, 
     getRecipe, 
     getPicture,
